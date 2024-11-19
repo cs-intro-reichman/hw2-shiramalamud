76eafe867129879fcdb85cf45662961e918dc9ff
shiramalamud <shira.malamud@post.runi.ac.il>
2024-11-19 22:59:20 +0200

    HW2

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,37 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+        public static void main(String[] args)
+        {
+                String name = args[0];
+                String newname="";
+                int i=0;
+                while(i<name.length())
+                {
+                        newname=newname+Character.toUpperCase(name.charAt(i));
+                        i++;
+                }
+                int num = Integer.parseInt(args[1]);
+                String letters="AEFHILMNORSX";
+                for(int j=0; j<newname.length(); j++)
+                {
+                        char ch=newname.charAt(j);
+                        if (letters.indexOf(ch)!=-1)
+                        {
+                                System.out.println("Give me an "+": "+ch+": "+ch+"!");
+                        }
+                        else
+                        {
+                                System.out.println("Give me a  "+": "+ch+": "+ch+"!");
+                        }
+                }
+                System.out.println("What does that spell?");
+                for (int j=0; j<num; j++)
+                {
+                        System.out.println(newname+"!!!");
+        
+                }
+
+
+	    
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,39 @@
-public class TimeCalc {
-    public static void main(String[] args) {
-        // Replace this comment with your code
+public class TimeCalc 
+{
+    public static void main(String[] args) 
+    {
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutestoadd=Integer.parseInt(args[1]);
+        int totalminutes=(hours*60)+minutes+minutestoadd;
+        int totalhours=totalminutes/60;
+        int newhours=totalhours%24;
+        int newminutes=totalminutes-(totalhours*60);
+        if(hours<10)
+        {
+            if(minutes<10)
+            {
+                System.out.println("0"+newhours+":0"+newminutes);
+            }
+            else
+            {
+                System.out.println(newhours+":0"+newminutes);
+            }
+        }
+        else
+        {
+            if(minutes<10)
+            {
+                System.out.println(newhours+":0"+newminutes);
+            }
+            else
+            {
+                System.out.println(newhours+":"+newminutes);
+
+            }
+        }
+        
+
+
     }
 }
